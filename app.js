053266be8dc06fe335b6dcf63f8a61ee27a14dd2
Dewan-toro <dewantoro477@gmail.com>
2024-01-03 07:36:42 +0700

    Membuat halaman create dan menyimpan data pace di db

--- app.js
+++ app.js
@@ -19,19 +19,32 @@ mongoose
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+//middleware
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get('/places', async(req, res) => {
+app.get("/places", async (req, res) => {
   const places = await Place.find();
-  res.render('places/index', {places})
-})
+  res.render("places/index", { places });
+});
 
-app.get('/places/:id', async(req, res) => {
+app.get("/places/create", (req, res) => {
+  res.render("places/create");
+});
+
+app.post("/places", async (req, res) => {
+  const place = new Place(req.body.place);
+  await place.save();
+  res.redirect("/places");
+});
+
+app.get("/places/:id", async (req, res) => {
   const place = await Place.findById(req.params.id);
-  res.render('places/show', {place})
-})
+  res.render("places/show", { place });
+});
 
 // app.get("/seed/place", async (req, res) => {
 //   const place = new Place({

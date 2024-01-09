d6ab6a8ed09d17a549604a2fc49a23c2ecba1c2f
Dewan-toro <dewantoro477@gmail.com>
2024-01-09 21:39:34 +0700

    Membuat middleware untuk validasi inputan

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const ejsMate = require("ejs-mate");
 const express = require("express");
 const ErrorHandler = require("./utils/ErrorHandler");
-const Joi = require("joi");
 const methodOverride = require("method-override");
 const wrapAsync = require("./utils/wrapAsync");
 const path = require("path");
@@ -10,6 +9,9 @@ const app = express();
 
 //models
 const Place = require("./models/place");
+
+//schemas
+const { placeSchema } = require("./schemas/place");
 
 //connect to mongoDB
 mongoose
@@ -29,6 +31,16 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
+const validatePlace = (req, res, next) => {
+  const { error } = placeSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map(el => el.message).join(",");
+    return next(new ErrorHandler(msg, 400));
+  } else {
+    next();
+  }
+};
+
 app.get("/", (req, res) => {
   res.render("home");
 });
@@ -47,23 +59,8 @@ app.get("/places/create", (req, res) => {
 
 app.post(
   "/places",
+  validatePlace,
   wrapAsync(async (req, res, next) => {
-    const placeSchema = Joi.object({
-      place: Joi.object({
-        title: Joi.string().required(),
-        location: Joi.string().required(),
-        description: Joi.string().required(),
-        price: Joi.number().min(0).required(),
-        image: Joi.string().required(),
-      }).required(),
-    });
-
-    const { error } = placeSchema.validate(req.body);
-    if (error) {
-      console.log(error);
-      next(new ErrorHandler(error, 400));
-    }
-
     const place = new Place(req.body.place);
     await place.save();
     res.redirect("/places");
@@ -88,6 +85,7 @@ app.get(
 
 app.put(
   "/places/:id",
+  validatePlace,
   wrapAsync(async (req, res) => {
     await Place.findByIdAndUpdate(req.params.id, { ...req.body.place });
     res.redirect("/places");

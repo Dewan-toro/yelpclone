f6f94b8f06b52f050b952a604854a5b7a80746bc
Dewan-toro <dewantoro477@gmail.com>
2024-01-09 19:39:18 +0700

    Membuat validasi input di server side

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const ejsMate = require("ejs-mate");
 const express = require("express");
 const ErrorHandler = require("./utils/ErrorHandler");
+const Joi = require("joi");
 const methodOverride = require("method-override");
 const wrapAsync = require("./utils/wrapAsync");
 const path = require("path");
@@ -44,40 +45,71 @@ app.get("/places/create", (req, res) => {
   res.render("places/create");
 });
 
-app.post("/places", wrapAsync(async (req, res, next) => {
+app.post(
+  "/places",
+  wrapAsync(async (req, res, next) => {
+    const placeSchema = Joi.object({
+      place: Joi.object({
+        title: Joi.string().required(),
+        location: Joi.string().required(),
+        description: Joi.string().required(),
+        price: Joi.number().min(0).required(),
+        image: Joi.string().required(),
+      }).required(),
+    });
+
+    const { error } = placeSchema.validate(req.body);
+    if (error) {
+      console.log(error);
+      next(new ErrorHandler(error, 400));
+    }
+
     const place = new Place(req.body.place);
     await place.save();
     res.redirect("/places");
-}));
+  })
+);
 
-app.get("/places/:id", wrapAsync(async (req, res) => {
-  const place = await Place.findById(req.params.id);
-  res.render("places/show", { place });
-}));
+app.get(
+  "/places/:id",
+  wrapAsync(async (req, res) => {
+    const place = await Place.findById(req.params.id);
+    res.render("places/show", { place });
+  })
+);
 
-app.get("/places/:id/edit", wrapAsync(async (req, res) => {
-  const place = await Place.findById(req.params.id);
-  res.render("places/edit", { place });
-}));
+app.get(
+  "/places/:id/edit",
+  wrapAsync(async (req, res) => {
+    const place = await Place.findById(req.params.id);
+    res.render("places/edit", { place });
+  })
+);
 
-app.put("/places/:id", wrapAsync(async (req, res) => {
-  await Place.findByIdAndUpdate(req.params.id, { ...req.body.place });
-  res.redirect("/places");
-}));
+app.put(
+  "/places/:id",
+  wrapAsync(async (req, res) => {
+    await Place.findByIdAndUpdate(req.params.id, { ...req.body.place });
+    res.redirect("/places");
+  })
+);
 
-app.delete("/places/:id", wrapAsync(async (req, res) => {
-  await Place.findByIdAndDelete(req.params.id);
-  res.redirect("/places");
-}));
+app.delete(
+  "/places/:id",
+  wrapAsync(async (req, res) => {
+    await Place.findByIdAndDelete(req.params.id);
+    res.redirect("/places");
+  })
+);
 
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   next(new ErrorHandler("Page not found", 404));
-})
+});
 
 app.use((err, req, res, next) => {
-  const {statusCode = 500} = err;
-  if(!err.message) err.message = 'Oh no! Something went wrong'
-  res.status(statusCode).render('error',{err})
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Oh no! Something went wrong";
+  res.status(statusCode).render("error", { err });
 });
 
 app.listen(3000, () => {

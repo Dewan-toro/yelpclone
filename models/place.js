532e6bb866ad21163884b259c7f5e592bfa78041
Dewan-toro <dewantoro477@gmail.com>
2024-01-10 21:11:41 +0700

    Menghapus data parent place beserta data child review

--- models/place.js
+++ models/place.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review");
 
 const placeSchema = new Schema({
   title: String,
@@ -9,5 +10,15 @@ const placeSchema = new Schema({
   image: String,
   reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
 });
+
+placeSchema.post("findOneAndDelete", async function (doc) {
+  if(doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews
+      }
+    })
+  }
+})
 
 module.exports = mongoose.model("Place", placeSchema);
